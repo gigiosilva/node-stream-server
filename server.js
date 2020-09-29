43e3a0a2dc2f66bf8590432160543e6b88bc0069
Giovani Moraes <giovani@eaglecatch.com>
2020-09-29 18:02:37 -0300

    Teste rtmp

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const NodeMediaServer = require('node-media-server');
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const app = express()
-
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/index.html'))
-// })
+const RtmpServer = require('rtmp-server');
+const rtmpServer = new RtmpServer();
  
-const config = {
-  rtmp: {
-    port: 3000,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60
-  },
-  http: {
-    port: 8000,
-    allow_origin: '*'
-  }
-};
+rtmpServer.on('error', err => {
+  throw err;
+});
  
-var nms = new NodeMediaServer(config)
-nms.run();
-
-// app.listen(3000, function () {
-//   console.log('Listening on port 3000!')
-// })
+rtmpServer.on('client', client => {
+  //client.on('command', command => {
+  //  console.log(command.cmd, command);
+  //});
+ 
+  client.on('connect', () => {
+     console.log('connect', client.app);
+  });
+  
+  client.on('play', ({ streamName }) => {
+    console.log('PLAY', streamName);
+  });
+  
+  client.on('publish', ({ streamName }) => {
+    console.log('PUBLISH', streamName);
+  });
+  
+  client.on('stop', () => {
+    console.log('client disconnected');
+  });
+});
+ 
+rtmpServer.listen(3000);
