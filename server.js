fa93924009e4e5272c3c02bf2f12b1d24356fc14
Giovani Moraes <giovani@eaglecatch.com>
2020-09-29 17:02:20 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const NodeMediaServer = require('node-media-server');
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+ 
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
+ 
+var nms = new NodeMediaServer(config)
+nms.run();
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!')
+})
